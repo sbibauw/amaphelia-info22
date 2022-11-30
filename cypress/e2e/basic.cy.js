94b544556e56b511a833b9a6615f6c06e28a684f
Serge Bibauw <sbibauw@gmail.com>
2022-11-30 01:01:18 -0500

    update adaptation

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,31 +4,31 @@ describe('empty spec', () => {
   })
 
   it('opens the index page', () => {
-    cy.get('h1').contains('Great coffee with a conscience')
+    cy.get('h1').contains('AMAP Helia')
   })
 
-  it('navigates to the product page', () => {
-    cy.get('a[href="/products"]').eq(0).click();
-    cy.url().should("include", "/products")
-    cy.get('h1').contains(/Our Coffee/i)
+  it('navigates to the Principes page', () => {
+    cy.get('a[href="/principes"]').eq(0).click();
+    cy.url().should("include", "/principes")
+    cy.get('h1').contains(/Principes/i)
   })
 
-  it('navigates to the values page', () => {
-    cy.get('a[href="/values"]').eq(0).click();
-    cy.url().should("include", "/values")
-    cy.get('h1').contains(/Values/i)
-  })
+  // it('navigates to the values page', () => {
+  //   cy.get('a[href="/values"]').eq(0).click();
+  //   cy.url().should("include", "/values")
+  //   cy.get('h1').contains(/Values/i)
+  // })
 
-  it('navigates to the blog page', () => {
-    cy.get('a[href="/post"]').eq(0).click();
-    cy.url().should("include", "/post")
-    cy.get('h1').contains(/Latest Stories/i)
-  })
-})
+  //   it('navigates to the blog page', () => {
+  //     cy.get('a[href="/post"]').eq(0).click();
+  //     cy.url().should("include", "/post")
+  //     cy.get('h1').contains(/Latest Stories/i)
+  //   })
+  // })
 
-describe('validate blog', () => {
-  it('should have only 3 blog posts by default', () => {
-    cy.visit('/post')
-    cy.get('ul#blog-list li').should('have.length', 3);
+describe('validate Nouvelles', () => {
+  it('should have only 3 posts', () => {
+    cy.visit('/nouvelles')
+    cy.get('ul#blog-list li').should('have.length', 4);
   })
 })
