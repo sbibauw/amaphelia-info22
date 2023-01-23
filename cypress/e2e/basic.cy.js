9f4975907ce872cc355952c707122012bd7e0cee
Serge Bibauw <sbibauw@gmail.com>
2023-01-22 21:59:44 -0500

    fix cypress tests

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -19,15 +19,15 @@ describe('empty spec', () => {
   //   cy.get('h1').contains(/Values/i)
   // })
 
-  //   it('navigates to the blog page', () => {
-  //     cy.get('a[href="/post"]').eq(0).click();
-  //     cy.url().should("include", "/post")
-  //     cy.get('h1').contains(/Latest Stories/i)
-  //   })
+  // it('navigates to the blog page', () => {
+  //   cy.get('a[href="/post"]').eq(0).click();
+  //   cy.url().should("include", "/post")
+  //   cy.get('h1').contains(/Latest Stories/i)
   // })
+  })
 
 describe('validate Nouvelles', () => {
-  it('should have only 3 posts', () => {
+  it('should have only 4 posts', () => {
     cy.visit('/nouvelles')
     cy.get('ul#blog-list li').should('have.length', 4);
   })
