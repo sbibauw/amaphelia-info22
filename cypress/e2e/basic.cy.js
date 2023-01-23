974416bf485c99ff19fb92ab504cd42e7b2e4f19
Serge Bibauw <sbibauw@gmail.com>
2023-01-22 22:19:24 -0500

    fix: nouvelles as <li> + cypress test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -13,22 +13,16 @@ describe('empty spec', () => {
     cy.get('h1').contains(/Principes/i)
   })
 
-  // it('navigates to the values page', () => {
-  //   cy.get('a[href="/values"]').eq(0).click();
-  //   cy.url().should("include", "/values")
-  //   cy.get('h1').contains(/Values/i)
-  // })
-
-  // it('navigates to the blog page', () => {
-  //   cy.get('a[href="/post"]').eq(0).click();
-  //   cy.url().should("include", "/post")
-  //   cy.get('h1').contains(/Latest Stories/i)
-  // })
+  it('navigates to the Nouvelles page', () => {
+    cy.get('a[href="/nouvelles"]').eq(0).click();
+    cy.url().should("include", "/nouvelles")
+    cy.get('h1').contains(/Dernières nouvelles/i)
   })
+})
 
 describe('validate Nouvelles', () => {
-  it('should have only 4 posts', () => {
+  it('should have 7 posts', () => {
     cy.visit('/nouvelles')
-    cy.get('ul#blog-list li').should('have.length', 4);
+    cy.get('ul#blog-list li').should('have.length', 7);
   })
 })
